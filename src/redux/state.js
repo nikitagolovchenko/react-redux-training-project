8e72c6141bd61f55c869fa7aede6eb040f9e13f5
Nikita Golovchenko <sunburst1715@gmail.com>
2020-01-28 02:01:55 +0200

    OOP store this\bind

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,79 +1,86 @@
-let renderEntireTree = () => {  // заглушка
-    console.log('State was changed');
-}
-
-let state = {
-	profilePage: {
-        posts: [
-            { id: 1, message: 'Hello World!', likesCount: 5 },
-            { id: 2, message: "It's my first project", likesCount: 12 },
-        ],
-
-        newPostText: "New post text"
+let store = {
+    // приватное свойство:
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hello World!', likesCount: 5 },
+                { id: 2, message: "It's my first project", likesCount: 12 },
+            ],
+    
+            newPostText: "New post text"
+        },
+    
+        dialogsPage: {
+            messagesData: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How are you?' },
+                { id: 3, message: 'Yo' },
+                { id: 4, message: 'Good bye' },
+                { id: 5, message: 'Bye' },
+            ],
+    
+            dialogsData: [
+                { id: 1, name: 'Nikita' },
+                { id: 2, name: 'Oleg' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+            ],
+            // ------
+            enteredText: ''
+        }
     },
 
-	dialogsPage: {
-        messagesData: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How are you?' },
-            { id: 3, message: 'Yo' },
-            { id: 4, message: 'Good bye' },
-            { id: 5, message: 'Bye' },
-        ],
+    getState() {
+        return this._state;
+    },
 
-        dialogsData: [
-            { id: 1, name: 'Nikita' },
-            { id: 2, name: 'Oleg' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-        ],
-        // ------
-        enteredText: ''
+    // приватный метод:
+    _callSubscriber() {  // заглушка
+        console.log('State was changed');
+    },
+
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+    
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    // добавить введенное значение (сохраненное в state) в массив сообщений и обнулить введеное значение:
+    addNewMessage() {
+        let newMessage = {
+            id: 6,
+            message: this._state.dialogsPage.enteredText,
+        };
+    
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.enteredText = '';
+        this._callSubscriber(this._state);
+    },
+
+    // добавить введенное значение в state:
+    updateEnteredText(newText) {
+        this._state.dialogsPage.enteredText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;  // паттерн observer(наблюдатель)
     }
-};
 
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
 }
 
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-
-// ----======00000=====-------
-// добавить введенное значение (сохраненное в state) в массив сообщений и обнулить введеное значение:
-export const addNewMessage = () => {
-    let newMessage = {
-        id: 6,
-        message: state.dialogsPage.enteredText,
-    };
-
-    state.dialogsPage.messagesData.push(newMessage);
-    state.dialogsPage.enteredText = '';
-    renderEntireTree(state);
-}
-
-// добавить введенное значение в state:
-export const updateEnteredText = (newText) => {
-    state.dialogsPage.enteredText = newText;
-    renderEntireTree(state);
-}
-
-// ================================
-export const subscribe = (observer) => {
-    renderEntireTree = observer;  // паттерн observer(наблюдатель)
-}
-
-export default state;
+export default store;
+window.store = store;
